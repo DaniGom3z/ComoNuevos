a6a96525fb0583e0566b0f1cda76ebac0e88bb93
DaniGom3z <danielhernandezgomez103@gmail.com>
2023-10-25 18:24:02 -0600

    front en react

--- src/app/components/atoms/input/input.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface InputProps {
-  texto: string;
-}
-
-export function Input({ texto }: InputProps) {
-  return <input type="text" className="w-64 p-2 rounded-sm" placeholder={texto} />;
-}
